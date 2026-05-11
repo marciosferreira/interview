6cd81d9ca0d7044930972e39d99b67eb5b0261d1
Marcio Ferreira <marcio.ferreira@fit-tecnologia.org.br>
2026-05-11 11:08:35 -0400

    ok

--- simulador-entrevista/static/analytics.js
+++ simulador-entrevista/static/analytics.js
@@ -1,4 +1,5 @@
 // Microsoft Clarity
+if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') return;
 (function(c,l,a,r,i,t,y){
     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
